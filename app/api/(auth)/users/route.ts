1ae89400fa94cd719e7932eaf525fd4096a88554
Nimesh8848 <nimeshmathema@gmail.com>
2024-11-24 12:01:06 +0545

    get and psot method for users made

--- app/api/(auth)/users/route.ts
+++ app/api/(auth)/users/route.ts
@@ -1,4 +1,33 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import connect from "@/lib/db";
+import User from "@/lib/modals/users";
 import {NextResponse} from "next/server";
-export const GET=()=>{
- return  new NextResponse("This is my first api");
-}
+export const GET = async () => {
+    try {
+      await connect();
+      const users = await User.find();
+      return new NextResponse(JSON.stringify(users), { status: 200 });
+    } catch (error: any) {
+      return new NextResponse("Error in fetching users" + error.message, {
+        status: 500,
+      });
+    }
+  };
+
+  export const POST = async (request: Request) => {
+    try {
+      const body = await request.json();
+      await connect();
+      const newUser = new User(body);
+      await newUser.save();
+  
+      return new NextResponse(
+        JSON.stringify({ message: "User is created", user: newUser }),
+        { status: 200 }
+      );
+    } catch (error: any) {
+      return new NextResponse("Error in creating user" + error.message, {
+        status: 500,
+      });
+    }
+  };
