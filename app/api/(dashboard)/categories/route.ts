6794d78ecfefa7127546ddc924cb9a0a4982a502
Nimesh8848 <nimeshmathema@gmail.com>
2024-11-25 16:43:50 +0545

    get and post method for category completed

--- app/api/(dashboard)/categories/route.ts
+++ app/api/(dashboard)/categories/route.ts
@@ -5,29 +5,81 @@ import {NextResponse} from "next/server";
 import { Types } from "mongoose";
 import Category from "@/lib/modals/category";
 
-export const GET=async(request:Request)=>{
-   try {
-    const {searchParams}=new URL(request.url);
-    const userId=searchParams.get('userId');
-    if(!userId||Types.ObjectId.isValid(userId)){
+export const GET = async (request: Request) => {
+    try {
+      const { searchParams } = new URL(request.url);
+      const userId = searchParams.get("userId");
+  
+      if (!userId || !Types.ObjectId.isValid(userId)) {
         return new NextResponse(
-            JSON.stringify({message:'Invalid User ID'}),{status: 400}
+          JSON.stringify({ message: "Invalid or missing userId" }),
+          {
+            status: 400,
+          }
         );
-    }
-    await connect();
-    const user=await User.findById(new Types.ObjectId(userId));
-    if(!user){
+      }
+  
+      await connect();
+  
+      const user = await User.findById(userId);
+      if (!user) {
         return new NextResponse(
-            JSON.stringify({message:'User not found'}),{status: 404}
+          JSON.stringify({ message: "User not found in the database" }),
+          {
+            status: 400,
+          }
         );
+      }
+  
+      const categories = await Category.find({
+        user: new Types.ObjectId(userId),
+      });
+  
+      return new NextResponse(JSON.stringify(categories), {
+        status: 200,
+      });
+    } catch (error: any) {
+      return new NextResponse("Error in fetching categories" + error.message, {
+        status: 500,
+      });
     }
-     const categories=await Category.find({user:new Types.ObjectId(userId)});
-     return new NextResponse(
-        JSON.stringify({user,categories}),{status: 200}
-     );
-   } catch (error:any) {
-     return new NextResponse(
-        JSON.stringify({message:'Server Error',error}),{status: 500}
-     )
-   }  
-}
+  };
+
+  export const POST = async (request:Request) => {
+    try {
+      const { searchParams } = new URL(request.url);
+    const userId = searchParams.get("userId");
+    const {title}=await request.json();
+    if (!userId || !Types.ObjectId.isValid(userId)) {
+      return new NextResponse(
+        JSON.stringify({ message: "Invalid or missing userId" }),
+        {
+          status: 400,
+        }
+      );
+    }
+  await connect();
+  const user = await User.findById(userId);
+  if(!user) {
+    return new NextResponse(
+      JSON.stringify({ message: "User not found in the database" }),
+      {
+        status: 400,
+      }
+    );
+  }
+  const newCategory=new Category({
+    title,
+    user: new Types.ObjectId(userId)
+  });
+
+  await newCategory.save();
+  return new NextResponse(JSON.stringify({message:"category is created",category:newCategory}), {
+    status: 200,
+  });
+    } catch (error:any) {
+      return new NextResponse("Error in fetching categories" + error.message, {
+        status: 500,
+      });
+    }
+  }
